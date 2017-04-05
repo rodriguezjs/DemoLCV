e4239490eb6f4c2fd7846d07e935d146bde0dba5
rodriguezjs <rodriguezjsamuel@gmail.com>
2017-04-05 00:23:18 -0400

    primera agregacion de datos

--- DemoMvcLCV/DemoMvcLCV/Controllers/AccountController.cs
+++ DemoMvcLCV/DemoMvcLCV/Controllers/AccountController.cs
@@ -8,10 +8,10 @@ using System.Web.Security;
 using DotNetOpenAuth.AspNet;
 using Microsoft.Web.WebPages.OAuth;
 using WebMatrix.WebData;
-using MvcLCV.Filters;
-using MvcLCV.Models;
+using DemoMvcLCV.Filters;
+using DemoMvcLCV.Models;
 
-namespace MvcLCV.Controllers
+namespace DemoMvcLCV.Controllers
 {
     [Authorize]
     [InitializeSimpleMembership]
